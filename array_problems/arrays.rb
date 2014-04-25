632109e456ac376ef092e413f4cfde6ea90da264
Amy Bakameyer <bymak87@gmail.com>
2014-04-25 14:07:39 -0500

    passes all the .max tests

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,6 +3,15 @@ require 'pry-debugger'
 
 module ArrayUtil
   def self.max(array)
+      if array.length == 0
+        return nil
+      elsif array.length == 1
+        array.first
+      else
+        array.sort! {|x,y| y<=>x}
+        array.first
+
+      end
   end
 
   def self.middle_element(array)
