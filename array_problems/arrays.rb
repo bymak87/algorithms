8d74cf708db6c3d0e270581b513bc33fda89afc0
Amy Bakameyer <bymak87@gmail.com>
2014-04-26 12:51:42 -0500

    passes all tests

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -10,13 +10,38 @@ module ArrayUtil
       else
         array.sort! {|x,y| y<=>x}
         array.first
-
       end
   end
 
   def self.middle_element(array)
+    value = (array.length/2)
+      if array.length == 0
+        return nil
+      elsif array.length == 1
+        array.first
+      elsif array.length % 2 != 0
+        array[value.ceil]
+      else
+        sum = (array[value] + array[value-1])
+        avg = sum.fdiv(2)
+        avg
+      end
   end
 
   def self.sum_arrays(array1, array2)
+    arraySum = []
+    i = 0
+    if array1.length == 0 && array2.length == 0
+      arraySum
+    elsif array1.length == 1 && array2.length == 1
+      arraySum << array1.first + array2.first
+      arraySum
+    else
+      while i < (array1.length)
+        arraySum << array1[i] + array2[i]
+        i+=1
+      end
+      arraySum
+    end
   end
 end
